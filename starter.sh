b65b5a80606c2e8cd1c38f9da680e1b79173d5a5
Unknown <inaciosk8@gmail.com>
2017-11-14 23:55:27 -0200

    fixed default user and password

--- starter.sh
+++ starter.sh
@@ -6,8 +6,8 @@ USER=${BITCOIN_USER:-codegik}
 PASS=${BITCOIN_PASS:-codegik}
 CONFIG=/bitcoin.$ENVIRONMENT.conf
 
-sed -i -- "s/user/$USER/g" "$CONFIG"
-sed -i -- "s/pass/$PASS/g" "$CONFIG"
+sed -i -- "s/USER/$USER/g" "$CONFIG"
+sed -i -- "s/PASS/$PASS/g" "$CONFIG"
 
 /etc/init.d/rabbitmq-server start
 
